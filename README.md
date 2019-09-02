d4c0bed898fc66d77116a615eb0792bc0082409d
GeraltShi <paladin.heleth@gmail.com>
2019-09-02 15:43:08 +0800

    support command line input of id and lyric type, add txt file save and music info support

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Enceladus
 Get NetEase music lyrics with Python3
+
+Usage: `lyric.py [-h] id format`
+positional arguments:
+  id          id of the music
+  format      choose the format of the output: orig/trans/merge
+
+optional arguments:
+  -h, --help  show this help message and exit
+
+orig: original lyrics
+trans: translated lyrics
+merge: lyrics in "[original]/[translated]" type
+
+For example: `python3 lyric.py 533943763 merge`
+This would generate a txt lyric that meets the `lrc` file standard under the script executing folder.
+You may change the extension from `*.txt` to `*.lrc` manually if you wanna import it in other apps.
+
+If this program helps with your MAD/AMV production, please do not hesitate to give me stars. :smile:
--- lyric.py
+++ lyric.py
@@ -2,6 +2,7 @@ import re
 import requests
 import json
 import argparse
+import sys
 
 class LyricParser:
     def __init__(self, url):
@@ -12,6 +13,9 @@ class LyricParser:
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0'}
         self.lyric = ''
         self.tlyric = ''
+        self.title = ''
+        self.artists = ''
+        self.album = ''
 
     def get_song_id(self):
         if self.url != "":
@@ -19,6 +23,19 @@ class LyricParser:
             return id
         else:
             return ""
+
+    def get_info(self):
+        id = self.get_song_id()
+        if id != "":
+            link = "http://music.163.com/api/song/detail/?id=" + id + "&ids=%5B" + id + "%5D"
+            web_data = requests.get(url=link, headers=self.headers).text
+            json_data = json.loads(web_data)
+            self.title = json_data['songs'][0]['name']
+            self.artists = json_data['songs'][0]['artists'][0]['name']
+            self.album = json_data['songs'][0]['album']['name']
+            print(self.title)
+            print(self.artists)
+            print(self.album)
 
     def get_lyrics(self):
         id = self.get_song_id()
@@ -36,7 +53,7 @@ class LyricParser:
         else:
             return "link error, please try again"
 
-    def merge_lyrics(self):
+    def process_lyrics(self):
         # Step 1: convert lyric to dict
         # print(self.lyric)
         lyric_dict = {}
@@ -63,15 +80,28 @@ class LyricParser:
         # print(merge_dict)
 
         # Step 4: print the merged dict in lyric format
-        for k in sorted(merge_dict):
-            print(k + "/".join(merge_dict[k]))
+        return lyric_dict, tlyric_dict, merge_dict     
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("format")
-    # args = parser.parse_args()
-    # print(args.format)
-    url = 'https://music.163.com/#/song?id=533943763'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("id", help="id of the music", type=str)
+    parser.add_argument("format", help="choose the format of the output: orig/trans/merge", type=str)
+    args = parser.parse_args()
+    url = 'https://music.163.com/#/song?id=' + args.id
     net = LyricParser(url)
+    net.get_info()
     lyric, tlyric = net.get_lyrics()
-    net.merge_lyrics()
+    lyric_dict, tlyric_dict, merge_dict = net.process_lyrics()
+    doc = open(net.title + '-' + net.artists + '-' + net.album + '.txt', 'w')
+    if args.format == "merge":
+        for k in sorted(merge_dict):
+            print(k + "/".join(merge_dict[k]))
+            print(k + "/".join(merge_dict[k]), file=doc)
+    if args.format == "orig":
+        for k in sorted(lyric_dict):
+            print(k + lyric_dict[k])
+            print(k + lyric_dict[k], file=doc)
+    if args.format == "trans":
+        for k in sorted(tlyric_dict):
+            print(k + tlyric_dict[k])
+            print(k + tlyric_dict[k], file=doc)
